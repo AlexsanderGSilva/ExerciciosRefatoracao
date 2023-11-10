48414fff5dafca733a22aca35486a093fcecd7eb
Alexsander Gonçalves da Silva <alexsander.gon.silva@gmail.com>
2023-11-09 21:23:54 -0300

    Initial commit

---
+++ ExerciciosRefatoracao/UI/CadastroPJ.cs
@@ -0,0 +1,19 @@
+﻿using ExerciciosRefatoracao.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosRefatoracao.UI
+{
+    public class CadastroPJ
+    {
+        ClienteRepository _repository;
+        public CadastroPJ(ClienteRepository repository) { this._repository = repository; }
+        public void Load()
+        {
+
+        }
+    }
+}
